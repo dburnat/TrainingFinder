398846e731e106a1394e0b284dc3aebfc1a91e9b
Szymon Domalik <szymon.domalik@gmail.com>
2020-04-02 21:22:11 +0200

    Added Admin Controller with method to view gyms in list

---
+++ TrainingFinder/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TrainingFinder.Data;
+
+namespace TrainingFinder.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IGymRepository _gymRepository;
+        public AdminController(IGymRepository gymRepository)
+        {
+            _gymRepository = gymRepository;
+        }
+        public IActionResult List() => View(_gymRepository.Gyms);
+        
+
+
+    }
+}
