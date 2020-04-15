36819446f1bf7357bf71b6444a3dcbdd1b8928c4
dburnat <d.burnat97@gmail.com>
2020-04-15 23:45:28 +0200

    Add view of all mobile users data

---
+++ TrainingFinder/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TrainingFinder.Data;
+using TrainingFinder.Models.Users;
+
+namespace TrainingFinder.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private IUserRepository _userRepository;
+
+        public UserController( IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public ViewResult Index() => View(_userRepository.GetAll());
+    }
+}
