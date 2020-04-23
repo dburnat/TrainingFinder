977ff7f502bf076257e85da49539eb06cf208ee6
dburnat <d.burnat97@gmail.com>
2020-04-23 22:07:14 +0200

    Add createGym POST method

--- TrainingFinderMobile/src/app/components/gymcreate/gymcreate.ts
+++ TrainingFinderMobile/src/app/components/gymcreate/gymcreate.ts
@@ -1,23 +1,53 @@
-import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+import { environment } from "./../../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import * as Toast from "nativescript-toast";
+import { zip } from "rxjs";
 
 @Component({
-    selector: 'gym',
-    templateUrl: 'gymcreate.html',
-    styleUrls: ['gymcreate.css']
+    selector: "gym",
+    templateUrl: "gymcreate.html",
+    styleUrls: ["gymcreate.css"],
 })
 export class GymCreateComponent implements OnInit {
-    constructor(private router: Router) { }
+    constructor(private http: HttpClient, private router: Router) {}
 
-    ngOnInit(): void { }
+    ngOnInit(): void {}
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
     }
 
-    createGym(){
-        
+    createGym(
+        gymName: string,
+        street: string,
+        number: string,
+        zipCode: string,
+        city: string
+    ) {
+        this.http
+            .post(`${environment.apiUrl}/gym/create`, {
+                gymName: gymName,
+                street: street,
+                number: number,
+                zipCode: zipCode,
+                city: city,
+            })
+            .subscribe(
+                (result) => {
+                    Toast.makeText(
+                        "Gym created. We will moderate it now.",
+                        "long"
+                    ).show();
+                    this.router.navigate(["login"]);
+                },
+                (error) => {
+                    Toast.makeText(error.message, "long").show();
+                    console.log(error);
+                }
+            );
     }
 }
