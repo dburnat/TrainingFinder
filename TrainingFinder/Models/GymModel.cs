e87f2eaf7b7e83172586230eff79898ae16f5acb
Szymon Domalik <szymon.domalik@gmail.com>
2020-04-24 12:54:07 +0200

    Created GymModel

---
+++ TrainingFinder/Models/GymModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingFinder.Models
+{
+    public class GymModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "City")]
+        public string City { get; set; }
+        [Required]
+        [Display(Name = "Street")]
+        public string Street { get; set; }
+        [Required]
+        [Display(Name = "Number")]
+        public string Number { get; set; }
+
+        [Display(Name = "PostCode")]
+        public string PostCode { get; set; }
+    }
+}
