898f087f5578ec9fafd9232d3fbf484a21d095ec
Szymon Domalik <szymon.domalik@gmail.com>
2020-05-04 13:50:37 +0200

    Created trainingModel for update and create methods in api

---
+++ TrainingFinder/Models/TrainingModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingFinder.Models
+{
+    public class TrainingModel
+    {
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        [Required]
+        [Display(Name = "Date and time")]
+        public DateTime DateTime { get; set; }
+        public int GymId { get; set; }
+    }
+}
