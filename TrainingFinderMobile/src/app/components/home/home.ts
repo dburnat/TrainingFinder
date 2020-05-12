ae771768e7bc4223667bedceeba7a87a2f821782
dburnat <d.burnat97@gmail.com>
2020-05-12 19:12:40 +0200

    Change in home layout

--- TrainingFinderMobile/src/app/components/home/home.ts
+++ TrainingFinderMobile/src/app/components/home/home.ts
@@ -1,25 +1,38 @@
-import { AuthenticationService } from './../../services/authentication.service';
+import { HttpClient } from "@angular/common/http";
+import { AuthenticationService } from "./../../services/authentication.service";
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
+import { environment } from "~/environments/environment";
+import { Gym } from "~/app/models/gym.model";
+import { ObservableArray } from "tns-core-modules/data/observable-array/observable-array";
+import { CardView } from 'nativescript-cardview';
+import { registerElement } from 'nativescript-angular/element-registry';
+registerElement('CardView', () => CardView);
+
 
 @Component({
     selector: "Home",
-    templateUrl: "home.html"
-
+    templateUrl: "home.html",
 })
 export class HomeComponent implements OnInit {
-
-    constructor(public authenticationService: AuthenticationService) {
+    constructor(
+        public authenticationService: AuthenticationService,
+        private http: HttpClient
+    ) {
         // Use the component constructor to inject providers.
     }
     public user = this.authenticationService.currentUserValue;
+    public gyms: ObservableArray<Gym>;
 
-    ngOnInit(): void {
-    }
+    ngOnInit(): void {}
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
+    }
+
+    getGymsFromApi() {
+        return this.http.get<Gym[]>(`${environment.apiUrl}/api/gym`);
     }
 }
