deb751c35e18fb6a535ff070fb7bcb66440e8b1b
dburnat <d.burnat97@gmail.com>
2020-05-12 23:14:05 +0200

    Add option to show current location in Google Maps

--- TrainingFinderMobile/src/app/components/home/home.ts
+++ TrainingFinderMobile/src/app/components/home/home.ts
@@ -1,3 +1,5 @@
+import { AppDataService } from "./../../services/appdata.service";
+import { userLocation } from "./../../models/userlocation.model";
 import { HttpClient } from "@angular/common/http";
 import { AuthenticationService } from "./../../services/authentication.service";
 import { Component, OnInit } from "@angular/core";
@@ -6,11 +8,13 @@ import * as app from "tns-core-modules/application";
 import { environment } from "~/environments/environment";
 import { Gym } from "~/app/models/gym.model";
 import { ObservableArray } from "tns-core-modules/data/observable-array/observable-array";
-import { CardView } from 'nativescript-cardview';
-import { registerElement } from 'nativescript-angular/element-registry';
-import { MapView } from 'nativescript-google-maps-sdk';
-registerElement('CardView', () => CardView);
+import { CardView } from "nativescript-cardview";
+import { registerElement } from "nativescript-angular/element-registry";
+import { MapView, Marker, Position } from "nativescript-google-maps-sdk";
+import { isAndroid, isIOS } from "tns-core-modules/ui/page/page";
+registerElement("CardView", () => CardView);
 registerElement("MapView", () => MapView);
+import * as geolocation from "nativescript-geolocation";
 @Component({
     selector: "Home",
     templateUrl: "home.html",
@@ -18,14 +22,18 @@ registerElement("MapView", () => MapView);
 export class HomeComponent implements OnInit {
     constructor(
         public authenticationService: AuthenticationService,
-        private http: HttpClient
+        private http: HttpClient,
+        private appDataService: AppDataService
     ) {
         // Use the component constructor to inject providers.
     }
     public user = this.authenticationService.currentUserValue;
     public gyms: ObservableArray<Gym>;
+    public userLocation: userLocation;
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.userLocation = this.appDataService.retrieveLocation();
+    }
 
     onDrawerButtonTap(): void {
         const sideDrawer = <RadSideDrawer>app.getRootView();
@@ -39,11 +47,13 @@ export class HomeComponent implements OnInit {
     onMapReady = (event) => {
         let mapView = event.object as MapView;
 
-        const CRACOW_CENTER_LATITUDE = 50.061585;
-        const CRACOW_CENTER_LONGITUDE = 19.936552;
+        const POLAND_CENTER_LATITUDE = 52;
+        const POLAND_CENTER_LONGITUDE = 19;
 
-        mapView.latitude = CRACOW_CENTER_LATITUDE;
-        mapView.longitude = CRACOW_CENTER_LONGITUDE;
-        mapView.zoom = 10;
-	}
+        mapView.latitude = POLAND_CENTER_LATITUDE;
+        mapView.longitude = POLAND_CENTER_LONGITUDE;
+        mapView.zoom = 5;
+        mapView.myLocationEnabled = true;
+        mapView.settings.zoomGesturesEnabled = true;
+    };
 }
--- TrainingFinderMobile/src/app/components/login/login.ts
+++ TrainingFinderMobile/src/app/components/login/login.ts
@@ -1,20 +1,21 @@
+import { AppDataService } from "./../../services/appdata.service";
 import { AuthenticationService } from "./../../services/authentication.service";
 import { Router, ActivatedRoute } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { first } from "rxjs/operators";
+import * as geolocation from "nativescript-geolocation";
+import { Accuracy } from "tns-core-modules/ui/enums";
 
 @Component({
     selector: "login",
     templateUrl: "login.html",
 })
 export class LoginComponent implements OnInit {
-
     public constructor(
-        private httpClient: HttpClient,
         private router: Router,
-        private route: ActivatedRoute,
-        private authenticationService: AuthenticationService
+        private authenticationService: AuthenticationService,
+        private appDataService: AppDataService
     ) {
         // redirect to home if already logged in
         if (this.authenticationService.currentUserValue) {
@@ -29,17 +30,66 @@ export class LoginComponent implements OnInit {
             .login(userName, password)
             .pipe(first())
             .subscribe(
-                result => {
+                (result) => {
                     this.router.navigate(["home"]);
                 },
-                error => {
+                (error) => {
                     console.log(error);
                     // this.loading = false;
-                });
+                }
+            );
     }
     public goToRegister() {
         this.router.navigate(["register"]);
     }
 
-    ngOnInit(): void {}
+    private getLocationPermission() {
+        geolocation.isEnabled().then(
+            function (isEnabled) {
+                if (!isEnabled) {
+                    geolocation
+                        .enableLocationRequest(true, true)
+                        .then(
+                            () => {
+                                console.log("User Enabled Location Service");
+                            },
+                            (e) => {
+                                console.log("Error: " + (e.message || e));
+                            }
+                        )
+                        .catch((ex) => {
+                            console.log("Unable to Enable Location", ex);
+                        });
+                }
+            },
+            function (e) {
+                console.log("Error: " + (e.message || e));
+            }
+        );
+    }
+
+    private saveCurrentLocation() {
+        let that = this;
+        geolocation
+            .getCurrentLocation({
+                desiredAccuracy: Accuracy.high,
+                maximumAge: 5000,
+                timeout: 10000,
+            })
+            .then(
+                function (loc) {
+                    if (loc) {
+                        that.appDataService.saveLocation(loc);
+                    }
+                },
+                function (e) {
+                    console.log("Error: " + (e.message || e));
+                }
+            );
+    }
+
+    ngOnInit(): void {
+        this.getLocationPermission();
+        this.saveCurrentLocation();
+    }
 }
