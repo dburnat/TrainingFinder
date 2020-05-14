1a3bac2447821ecbdc64cdd1741b8087d7bd6266
dburnat <d.burnat97@gmail.com>
2020-05-14 21:22:49 +0200

    Update login view with Toasts

--- TrainingFinderMobile/src/app/components/login/login.ts
+++ TrainingFinderMobile/src/app/components/login/login.ts
@@ -2,16 +2,15 @@ import { AppDataService } from "./../../services/appdata.service";
 import { AuthenticationService } from "./../../services/authentication.service";
 import { Router, ActivatedRoute } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
+import { Component} from "@angular/core";
 import { first } from "rxjs/operators";
-import * as geolocation from "nativescript-geolocation";
-import { Accuracy } from "tns-core-modules/ui/enums";
+import * as Toast from "nativescript-toast";
 
 @Component({
     selector: "login",
     templateUrl: "login.html",
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
     public constructor(
         private router: Router,
         private authenticationService: AuthenticationService,
@@ -28,12 +27,12 @@ export class LoginComponent implements OnInit {
 
         return this.authenticationService
             .login(userName, password)
-            .pipe(first())
             .subscribe(
                 (result) => {
                     this.router.navigate(["home"]);
                 },
                 (error) => {
+                    Toast.makeText(error.message, "long").show();
                     console.log(error);
                     // this.loading = false;
                 }
@@ -41,55 +40,5 @@ export class LoginComponent implements OnInit {
     }
     public goToRegister() {
         this.router.navigate(["register"]);
-    }
-
-    private getLocationPermission() {
-        geolocation.isEnabled().then(
-            function (isEnabled) {
-                if (!isEnabled) {
-                    geolocation
-                        .enableLocationRequest(true, true)
-                        .then(
-                            () => {
-                                console.log("User Enabled Location Service");
-                            },
-                            (e) => {
-                                console.log("Error: " + (e.message || e));
-                            }
-                        )
-                        .catch((ex) => {
-                            console.log("Unable to Enable Location", ex);
-                        });
-                }
-            },
-            function (e) {
-                console.log("Error: " + (e.message || e));
-            }
-        );
-    }
-
-    private saveCurrentLocation() {
-        let that = this;
-        geolocation
-            .getCurrentLocation({
-                desiredAccuracy: Accuracy.high,
-                maximumAge: 5000,
-                timeout: 10000,
-            })
-            .then(
-                function (loc) {
-                    if (loc) {
-                        that.appDataService.saveLocation(loc);
-                    }
-                },
-                function (e) {
-                    console.log("Error: " + (e.message || e));
-                }
-            );
-    }
-
-    ngOnInit(): void {
-        this.getLocationPermission();
-        this.saveCurrentLocation();
     }
 }
